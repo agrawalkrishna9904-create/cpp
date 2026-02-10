49d374a79e2a9e7fc3e62a16fd885853d24ff902
Kishna G Agrawal <agrawalkrishna9904@gmail.com>
2026-02-10 15:48:48 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "hello world" << endl;
+    cout << "hello world" <<"\n";
+    cout << "hello world " << "hello world" <<endl;
+    return 0;
+}
+
